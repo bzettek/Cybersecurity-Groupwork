6c53174267ef94ffd1c4163c449e7540a5e69d4b
Brandon Zettek <brandonzettek@Brandons-MacBook-Air.local>
2026-03-03 10:07:20 -0600

    fixed up code to match all files

--- asg3-350/AES_Timing.cpp
+++ asg3-350/AES_Timing.cpp
@@ -1,70 +1,73 @@
-/*
- * AES_Timing.cpp
- *
- * Brandon Zettek, Yusuf Oner, Jacob Doose, Mohammad Khan
- */
-
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
+//AES_Timing.cpp
+// Brandon Zettek, Yusuf Oner, Jacob Doose, Mohammad Khan
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 #include <openssl/aes.h>
 
 using namespace std;
 
-int main() {
-
-    const int LOOPS = 1000000;
-
-    // ------------------ Prepare AES Key ------------------
+int main() 
+{
+    // prepare AES key
     AES_KEY key;
-    unsigned char keyBytes[16] = {
-        0x01, 0x23, 0x45, 0x67,
-        0x89, 0xab, 0xcd, 0xef,
-        0x01, 0x23, 0x45, 0x67,
-        0x89, 0xab, 0xcd, 0xef
-    };
+    unsigned char keyBytes[] = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                    0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef };
 
-    // ------------------ Plaintext (exactly 16 bytes) ------------------
-    unsigned char plaintext[16] = {
-        's','e','c','r','e','t',' ',
-        'm','e','s','s','a','g','e',
-        '!','!'
-    };
-
-    // Buffers
-    unsigned char ciphertext[16];
-    unsigned char deciphertext[17];
-
-    clock_t start, end;
-
-    // ================= Encryption Timing =================
     AES_set_encrypt_key(keyBytes, 128, &key);
 
-    start = clock();
-    for (int i = 0; i < LOOPS; i++) {
+    // example input block: 16 bytes of data
+    unsigned char *plaintext = (unsigned char *)"Group 2! AES!!!";
+
+    // buffer to hold encrypted data
+    unsigned char ciphertext[16];
+
+    // timing
+    long all = 0;
+    int count = 1000; //how many times we encrypt
+    timespec start, stop; //for before and after encryption
+
+    // buffer to hold decrypted data
+    unsigned char deciphertext[17];
+
+    for (int i = 0; i < count; i++) 
+    {
+        clock_gettime(CLOCK_REALTIME, &start); //b4 encryption
+
+        // run encryption
         AES_ecb_encrypt(plaintext, ciphertext, &key, AES_ENCRYPT);
+
+        clock_gettime(CLOCK_REALTIME, &stop); //time right after encryption
+
+        //for nanosec conversion
+        long start_time = start.tv_sec * 1000000000 + start.tv_nsec; 
+        long stop_time = stop.tv_sec * 1000000000 + stop.tv_nsec;
+        all += stop_time - start_time; //running total
     }
-    end = clock();
 
-    double enc_time = double(end - start) / CLOCKS_PER_SEC;
-    double enc_avg = enc_time / LOOPS;
+    cout << "Average time used for AES encryption: " << (all/count) << " nanoseconds" << endl;
 
-    // ================= Decryption Timing =================
+    all = 0; //reset for decryption timing
+
     AES_set_decrypt_key(keyBytes, 128, &key);
 
-    start = clock();
-    for (int i = 0; i < LOOPS; i++) {
+    for (int i = 0; i < count; i++) 
+    {
+        clock_gettime(CLOCK_REALTIME, &start); //b4 encryption
+
+        // run decryption
         AES_ecb_encrypt(ciphertext, deciphertext, &key, AES_DECRYPT);
+        deciphertext[16] = '\0';
+
+        clock_gettime(CLOCK_REALTIME, &stop); //time right after encryption
+
+        //for nanosec conversion
+        long start_time = start.tv_sec * 1000000000 + start.tv_nsec; 
+        long stop_time = stop.tv_sec * 1000000000 + stop.tv_nsec;
+        all += stop_time - start_time; //running total
     }
-    end = clock();
 
-    double dec_time = double(end - start) / CLOCKS_PER_SEC;
-    double dec_avg = dec_time / LOOPS;
-
-    // Print Results
-    cout << "AES Encryption avg time: " << enc_avg << " seconds" << endl;
-    cout << "AES Decryption avg time: " << dec_avg << " seconds" << endl;
+    cout << "Average time used for AES decryption: " << (all/count) << " nanoseconds" << endl;
 
     return 0;
 }
